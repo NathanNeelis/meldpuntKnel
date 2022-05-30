7547b394ab2f2d29a89639724e06cb83ffbe27ee
Nathan Neelis <nathan.neelis@wearetriple.com>
2022-05-30 11:05:41 +0200

    something with severice worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,38 +1,78 @@
-const CORE_CACHE_VERSION = "1";
-const CORE_CACHE_NAME = "mk-cache" + CORE_CACHE_VERSION;
-const CORE_ASSETS = [
-  // assets to save
-  "/offline",
-];
+const CORE_CACHE_VERSION = "v2";
+const CORE_CACHE_NAME = "mk" + CORE_CACHE_VERSION;
+const CORE_ASSETS = ["/offline"];
 
 self.addEventListener("install", (event) => {
   console.log("install the service worker");
 
   // pre saves the offline page in the cache
-  // event.waitUntil(
-  //   caches.open(CORE_CACHE_NAME).then((cache) => {
-  //     return cache.addAll(CORE_ASSETS).then(() => self.skipWaiting());
-  //   })
-  // );
+  event.waitUntil(
+    caches.open(CORE_CACHE_NAME).then((cache) => {
+      return cache.addAll(CORE_ASSETS).then(() => self.skipWaiting());
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim()); // Service workers works direct instead after a reload in multiple browser tabs
+  // resource https://developer.mozilla.org/en-US/docs/Web/API/Clients/claim
   console.log("activate service worker. Removing old cache versions if needed");
 });
 
 self.addEventListener("fetch", (event) => {
   // do crazy cool things
-  // if (
-  //   event.request.method === "GET" &&
-  //   CORE_ASSETS.includes(getPathName(event.request.url))
-  // ) {
-  //   // cache only strategy --> CSS / JS / etc..
-  //   event.respondWith(
-  //     caches
-  //       .open(CORE_CACHE_NAME)
-  //       .then((cache) => cache.match(event.request.url))
-  //   );
-  // }
+
+  if (
+    event.request.method === "GET" &&
+    CORE_ASSETS.includes(getPathName(event.request.url))
+  ) {
+    // cache only strategy --> CSS / JS / etc..
+    event.respondWith(
+      caches
+        .open(CORE_CACHE_NAME)
+        .then((cache) => cache.match(event.request.url))
+    );
+  }
+
+  // MOVIE PAGE SPECIFIC
+  else if (
+    htmlGetRequest(event.request) &&
+    event.request.url.match("/movies")
+  ) {
+    // resource targetting url by client https://github.com/w3c/ServiceWorker/issues/985#issuecomment-253755588
+    // resource: https://stackoverflow.com/questions/45663796/setting-service-worker-to-exclude-certain-urls-only/45670014#45670014
+
+    // If there is internet, save the HTML to cache
+    // If there is no internet, find the cache and open the /movies page
+    event.respondWith(
+      fetchAndCache(event.request, "html-cache").catch(() => {
+        return caches
+          .open("html-cache")
+          .then((cache) => cache.match("/offline"));
+      })
+    );
+  }
+
+  // html get request
+  // If the page is in the chache, open the page through the cashe for faster loading
+  // Update the cache in the meanwhile, next page visits include the updates.
+  else if (htmlGetRequest(event.request)) {
+    event.respondWith(
+      caches
+        .open("html-cache")
+        .then((cache) => cache.match(event.request))
+        .then((response) =>
+          response ? response : fetchAndCache(event.request, "html-cache")
+        )
+        .catch(() => {
+          return caches
+            .open(CORE_CACHE_NAME)
+            .then((cache) => cache.match("/offline"));
+        })
+    );
+
+    event.waitUntil(fetchAndCache(event.request, "html-cache"));
+  }
 });
 
 // HELPER FUNCTIONS ----------------
