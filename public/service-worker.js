e35c615240e6aa1a7d7e9cb91b288ca32d626116
Nathan Neelis <nathan.neelis@wearetriple.com>
2022-05-30 11:01:20 +0200

    something with severice worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,19 @@
-const CORE_CACHE_VERSION = "v6";
+const CORE_CACHE_VERSION = "1";
 const CORE_CACHE_NAME = "mk-cache" + CORE_CACHE_VERSION;
 const CORE_ASSETS = [
   // assets to save
   "/offline",
-  "/images/app_icon/icon-192x192.png",
-  "/style/global.css",
 ];
 
 self.addEventListener("install", (event) => {
   console.log("install the service worker");
 
   // pre saves the offline page in the cache
-  event.waitUntil(
-    caches.open(CORE_CACHE_NAME).then((cache) => {
-      return cache.addAll(CORE_ASSETS).then(() => self.skipWaiting());
-    })
-  );
+  // event.waitUntil(
+  //   caches.open(CORE_CACHE_NAME).then((cache) => {
+  //     return cache.addAll(CORE_ASSETS).then(() => self.skipWaiting());
+  //   })
+  // );
 });
 
 self.addEventListener("activate", (event) => {
@@ -24,18 +22,17 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   // do crazy cool things
-
-  if (
-    event.request.method === "GET" &&
-    CORE_ASSETS.includes(getPathName(event.request.url))
-  ) {
-    // cache only strategy --> CSS / JS / etc..
-    event.respondWith(
-      caches
-        .open(CORE_CACHE_NAME)
-        .then((cache) => cache.match(event.request.url))
-    );
-  }
+  // if (
+  //   event.request.method === "GET" &&
+  //   CORE_ASSETS.includes(getPathName(event.request.url))
+  // ) {
+  //   // cache only strategy --> CSS / JS / etc..
+  //   event.respondWith(
+  //     caches
+  //       .open(CORE_CACHE_NAME)
+  //       .then((cache) => cache.match(event.request.url))
+  //   );
+  // }
 });
 
 // HELPER FUNCTIONS ----------------
